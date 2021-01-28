e6423d1ac320ce0ffc4a98ac004c243304b27861
Cami Wallace <cermy1995@byui.edu>
2021-01-27 22:44:47 -0700

    Initial commit

---
+++ src/HelloFromCami/Main.java
@@ -0,0 +1,9 @@
+package HelloFromCami;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Cami.");
+    }
+}
