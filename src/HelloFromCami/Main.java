254b3a0b6fa0d2dc844b223de7d6c01933cbb043
Timothy Eatough <u517514543@byui.edu>
2021-01-30 19:41:35 -0700

    Added my name

--- src/HelloFromCami/Main.java
+++ src/HelloFromCami/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Cami.");
+
+        System.out.println("Hello Cami, my name is Timothy.");
     }
 }
